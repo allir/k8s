642edb0faff2c651d0ec5d7dcf0cb529b7d95866
Aðalsteinn Rúnarsson <allir@wuxinextcode.com>
2018-11-13 15:48:56 +0000

    Update Lifecycle hello-app demo

--- examples/lifecycle/hello/main.go
+++ examples/lifecycle/hello/main.go
@@ -1,16 +1,26 @@
 package main
 
 import(
+	"os"
+	"log"
 	"fmt"
 	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+  title := os.Getenv("HELLO_VAR")
+  if title == "" {
+    title = "Ármúlason"
+  }
+	
+  log.Println("Writing response with title = " + title)
+	fmt.Fprintf(w, "Hello from " + title + "\nYou've requested %s\n", r.URL.Path) 
+}
+
 func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from Ármúlason.\n You've requested %s\n", r.URL.Path)
-    })
-
-		http.ListenAndServe(":8080", nil)
-
-	}
+  log.Println("Starting server, listening on port 8080")
+	
+  http.HandleFunc("/", handler)
+	log.Fatal( http.ListenAndServe(":8080", nil) )
+}
 
